db731d31cd999643bce234d0b3ffda52814158e1
Maylisten <Maylisten@users.noreply.github.com>
2026-07-11 22:45:30 +0800

    Initial Commit

---
+++ codex-menu-quota/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  codex-menu-quota
+//
+//  Created by ByteDance on 7/11/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ codex-menu-quota/codex_menu_quotaApp.swift
@@ -0,0 +1,17 @@
+//
+//  codex_menu_quotaApp.swift
+//  codex-menu-quota
+//
+//  Created by ByteDance on 7/11/26.
+//
+
+import SwiftUI
+
+@main
+struct codex_menu_quotaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
